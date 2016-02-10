c425b90df145da91522d704eafaf545c0176a8bf
peterchauyw <yuenwun.chau@hotmail.com>
2016-02-10 14:35:09 -0800

    .gitignore added

--- pyclient_cli.py
+++ pyclient_cli.py
@@ -18,14 +18,32 @@ import test
 
 
 parser = argparse.ArgumentParser(description="Command-line Interface for IoTDM Python Client")
-parser.add_argument('--op', '-o', help='operation', required=True)
-parser.add_argument("operation", choices=['restconf', 'kill', 'create', 'retrieve', 'update', 'delete'], help="Operation", type=str)
-parser.add_argument("url", help="Target URL", type=str)
-parser.add_argument("ty", help="Resource type", type=str, default=None)
-parser.add_argument("rn", help="Resource name", type=str, default=None)
+parser.add_argument('protocol', help='Protocol for Requests', choices=['http', 'coap'], type=str)
+parser.add_argument('--from', '-fr', help='From Which IP Address', type=str)
+parser.add_argument('--to', '-t', help='To Which IP Address', type=str)
+parser.add_argument('--name', '-nm', help='Resource Name', type=str)
+parser.add_argument('--op', '-o', help='Operation', choices=['restconf', 'kill', 'create', 'retrieve', 'update', 'delete'], type=str, required=True)
+parser.add_argument('--port', '-p', help='Port Number', type=str)
+parser.add_argument('--primitiveContent', '-pc', help='payload', type=str)
+parser.add_argument('--requestIdentifier', '-rqi', help='Request ID', type=str)
+parser.add_argument('--resourceType', '-ty', help='Resource Type', type=str)
+
 args = parser.parse_args()
-if args.operation =='restconf':
-    test.restConf(args.url, args.rn, 'admin', 'admin')
+
+url = args.protocol + '://' + args.
+
+# if args.operation == 'restconf':
+#     parser.add_argument("url", help="Target URL", type=str)
+#     parser.add_argument("CseName", help="CSE Name", type=str)
+#     args = parser.parse_args()
+#     test.restConf(args.url, args.CseName, 'admin', 'admin')
+
+# parser.add_argument("url", help="Target URL", type=str)
+# parser.add_argument("ty", help="Resource type", type=str, default=None)
+# parser.add_argument("rn", help="Resource name", type=str, default=None)
+# args = parser.parse_args()
+# if args.operation =='restconf':
+#     test.restConf(args.url, args.rn, 'admin', 'admin')
 
 
 
